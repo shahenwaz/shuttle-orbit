066b16957d8823ca255aab246dd2634fcc4dbc26
shahenwaz <kmuzahid52@gmail.com>
2026-05-05 02:12:35 +0100

    fix(prisma): generate client before production build

--- app/admin/tournaments/[tournamentId]/categories/[categoryId]/results/page.tsx
+++ app/admin/tournaments/[tournamentId]/categories/[categoryId]/results/page.tsx
@@ -36,7 +36,6 @@ export default async function AdminCategoryResultsPage({
         tournamentId,
       },
       include: {
-        knockoutStartStage: true,
         teamEntries: {
           orderBy: {
             createdAt: "asc",
